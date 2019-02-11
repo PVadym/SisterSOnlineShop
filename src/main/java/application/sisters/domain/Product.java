73c21b72b6d459c33d4bd3b96f111bdd0f64c63a
pylypchenko <pylypchenko>
2019-02-11 18:22:37 +0200

    added logging and JPA anotations for entities

--- src/main/java/application/sisters/domain/Product.java
+++ src/main/java/application/sisters/domain/Product.java
@@ -3,10 +3,16 @@ package application.sisters.domain;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 @Data
 @NoArgsConstructor
+@Entity
 public class Product extends BasicEntity {
     private String name;
     private Double price;
+    @Enumerated(value = EnumType.STRING)
     private Size size;
 }
